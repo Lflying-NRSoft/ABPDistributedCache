00a395bbe3c66b63ba652bb58c21bcc62ef365db
林飞 <fei.l@baisonmail.com>
2019-11-30 20:52:53 +0800

    upload code

---
+++ DistributedCache_GetOrAdd_ErrorDemo/AppModule.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.Caching;
+using Volo.Abp.Modularity;
+
+namespace DistributedCache_GetOrAdd_ErrorDemo
+{
+    [DependsOn(typeof(AbpAspNetCoreMvcModule))]
+    [DependsOn(typeof(AbpAutofacModule))] //Add dependency to ABP Autofac module
+    [DependsOn(typeof(AbpCachingModule))]
+    public class AppModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+
+            context.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration["Redis:Configuration"];
+            });
+        }
+        public override void OnApplicationInitialization(
+            ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseMvcWithDefaultRouteAndArea();
+        }
+    }
+}
---
+++ DistributedCache_GetOrAdd_ErrorDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Volo.Abp.Caching;
+
+namespace DistributedCache_GetOrAdd_ErrorDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        protected readonly IDistributedCache<WeatherForecast[]> _distributedCache;
+
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            IDistributedCache<WeatherForecast[]> distributedCache)
+        {
+            _logger = logger;
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> GetAsync()
+        {
+            var key = nameof(WeatherForecast);
+            return await _distributedCache.GetOrAddAsync(key, async () => await GetWeatherForecastAsync());
+        }
+
+        public Task<WeatherForecast[]> GetWeatherForecastAsync()
+        {
+            _logger.LogDebug("没有命中缓存");
+            var rng = new Random();
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return Task.FromResult(result);
+        }
+    }
+}
